ce08bec0d50a7218e1dc5166bf4e802d895f13af
jiyeon <a1919361@gmail.com>
2023-02-27 00:30:54 +0900

    chore : 프리티어 적용

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -2,10 +2,10 @@ module.exports = {
   printWidth: 100, // 기본값 80
   tabWidth: 2,
   singleQuote: true, // 기본값 false
-  trailingComma: "es5",
+  trailingComma: 'es5',
   semi: true,
   useTabs: false,
-  arrowParens: "always",
+  arrowParens: 'always',
   bracketSpacing: true,
   bracketSameLine: false,
 };
